0036e051a47c59b51bc70ac27ab9409f5530774d
eduxin <xyli@zjnu.cn>
2019-11-08 16:07:38 +0800

    test
    test website

---
+++ data/cache/memberlogin/c4/ca/42/c4ca4238a0b923820dcc509a6f75849b.php
@@ -0,0 +1,3 @@
+<?php exit('dedecms');?>
+
+a:2:{s:4:"data";a:22:{s:3:"mid";s:1:"1";s:5:"mtype";s:6:"个人";s:6:"userid";s:5:"admin";s:3:"pwd";s:32:"6b4bf8dc3dbd313f1d3a65b179498b4f";s:5:"uname";s:13:"www.ezhanku.com";s:3:"sex";s:3:"男";s:4:"rank";s:2:"10";s:6:"uptime";s:1:"0";s:7:"exptime";s:1:"0";s:5:"money";s:1:"0";s:5:"email";s:0:"";s:6:"scores";s:5:"10015";s:4:"matt";s:2:"10";s:8:"spacesta";s:1:"0";s:4:"face";s:0:"";s:12:"safequestion";s:1:"0";s:10:"safeanswer";s:0:"";s:8:"jointime";s:10:"1342219560";s:6:"joinip";s:0:"";s:9:"logintime";s:10:"1351266163";s:7:"loginip";s:9:"127.0.0.1";s:9:"checkmail";s:2:"-1";}s:7:"timeout";i:1352474088;}
